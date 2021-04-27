0435475131fecd6eea7e098804994ee979df67af
emran Muhammad aloul <78364500+emranaloul@users.noreply.github.com>
2021-04-27 13:50:33 +0300

    Update server.js
    fixing map function

--- server.js
+++ server.js
@@ -86,7 +86,7 @@ app.get( '/mycard', ( req,res )=>{
   let SQL = 'SELECT * FROM maybellinep;';
   client.query( SQL )
     .then( results =>{
-      let productsArr = results.body.map( val=>{
+      let productsArr = results.rows.map( val=>{
         return new Product( val );
       } );
       res.render( 'mycard', {results : productsArr} );
